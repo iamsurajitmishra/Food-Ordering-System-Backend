f081d13c2225112dac18f245448d8b983d031b2b
iamsurajitmishra <mishrasurajit69@gmail.com>
2024-06-08 12:44:31 +0530

    More service module added

---
+++ order-service/order-container/src/main/java/com/food/ordercontainer/OrderContainerApplication.java
@@ -0,0 +1,13 @@
+package com.food.ordercontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderContainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderContainerApplication.class, args);
+    }
+
+}
---
+++ order-service/order-container/src/test/java/com/food/ordercontainer/OrderContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.ordercontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderContainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
