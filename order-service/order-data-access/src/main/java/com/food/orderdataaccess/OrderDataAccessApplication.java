fa3314692d88e24384baf6b24226079b07416983
iamsurajitmishra <mishrasurajit69@gmail.com>
2024-06-08 10:43:04 +0530

    initial-commit

---
+++ order-service/order-data-access/src/main/java/com/food/orderdataaccess/OrderDataAccessApplication.java
@@ -0,0 +1,13 @@
+package com.food.orderdataaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDataAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderDataAccessApplication.class, args);
+    }
+
+}
---
+++ order-service/order-data-access/src/test/java/com/food/orderdataaccess/OrderDataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.orderdataaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDataAccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
